48f57c3024a24ddc5f577b0c878481d5dcc8a392
Tiago Baratieri <tiagobaratieri97@gmail.com>
2021-09-02 12:33:01 -0300

    Project created

---
+++ backend/src/test/java/com/tiagobaratieri/dsmovieflix/DsmovieflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagobaratieri.dsmovieflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
