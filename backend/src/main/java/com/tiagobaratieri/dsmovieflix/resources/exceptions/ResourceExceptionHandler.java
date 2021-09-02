0b3feb725631c984cc76e6bcd57451a61087f049
Tiago Baratieri <tiagobaratieri97@gmail.com>
2021-09-02 17:56:40 -0300

    Fix: resource exceptions

--- backend/src/main/java/com/tiagobaratieri/dsmovieflix/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/tiagobaratieri/dsmovieflix/resources/exceptions/ResourceExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.tiagobaratieri.dsmovieflix.resources.exceptions;
 
 
+import com.tiagobaratieri.dsmovieflix.services.exeptons.ForbiddenException;
 import com.tiagobaratieri.dsmovieflix.services.exeptons.ResourceNotFoundException;
+import com.tiagobaratieri.dsmovieflix.services.exeptons.UnauthorizedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -42,5 +44,17 @@ public class ResourceExceptionHandler {
         }
 
         return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(ForbiddenException.class)
+    public ResponseEntity<OAuthCustomError> forbidden(ForbiddenException e, HttpServletRequest request) {
+        OAuthCustomError err = new OAuthCustomError("Forbidden", e.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<OAuthCustomError> unauthorized(UnauthorizedException e, HttpServletRequest request) {
+        OAuthCustomError err = new OAuthCustomError("Unauthorized", e.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(err);
     }
 }
