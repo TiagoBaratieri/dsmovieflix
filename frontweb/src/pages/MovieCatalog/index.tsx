118426f4bf4a47bb214d6d3afd61559429a29d08
Tiago Baratieri <tiagobaratieri97@gmail.com>
2021-09-10 15:11:49 -0300

    created MovieCard, loaders

--- frontweb/src/pages/MovieCatalog/index.tsx
+++ frontweb/src/pages/MovieCatalog/index.tsx
@@ -1,12 +1,44 @@
+import MovieCard from 'components/MovieCard';
 import Navbar from 'components/Navbar';
 import { useEffect, useState } from 'react';
-import { Genre } from 'types/genre';
+import { Link } from 'react-router-dom';
+import { Genre } from 'types/Genre';
+import { MoviesResponse } from 'types/Movie';
 import { makePrivateRequest } from 'util/request';
+import MovieCardLoader from './components/MovieCardLoader';
 import './styles.css';
 
 const MovieCatalog = () => {
+  const [moviesReponse, setMoviesResponse] = useState<MoviesResponse>();
   const [genres, setGenres] = useState<Genre[]>([]);
   const [genreId, setGenreId] = useState(1);
+  const [activePage, setActivePage] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const params = {
+      page: activePage,
+      linesPerPage: 12,
+    };
+    setIsLoading(true);
+    makePrivateRequest({
+      url: `/movies?page=0&linesPerPage=12&direction=ASC&orderBy=title`,
+      params,
+    })
+      .then((response) => setMoviesResponse(response.data))
+      .finally(() => setIsLoading(false));
+  }, [activePage]);
+
+  useEffect(() => {
+    const params = {
+      page: activePage,
+      linesPerPage: 8,
+    };
+    setIsLoading(true);
+    makePrivateRequest({ url: `/movies?genreId=${genreId}`, params })
+      .then((response) => setMoviesResponse(response.data))
+      .finally(() => setIsLoading(false));
+  }, [genreId, activePage]);
 
   useEffect(() => {
     makePrivateRequest({ url: '/genres' }).then((response) =>
@@ -32,6 +64,24 @@ const MovieCatalog = () => {
             ))}
           </select>
         </form>
+        <div className="movie-card-container">
+          {isLoading ? (
+            <MovieCardLoader />
+          ) : (
+            <>
+              {moviesReponse?.content.map((movie) => (
+                <Link to={`movie/${movie.id}`} key={movie.id}>
+                  <MovieCard
+                    title={movie.title}
+                    subTitle={movie.subTitle}
+                    imgUrl={movie.imgUrl}
+                    year={movie.year}
+                  />
+                </Link>
+              ))}
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
