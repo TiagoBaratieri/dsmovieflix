f2bbf18b67fe5940c294c556bcb5aaed02dc988b
Tiago Baratieri <tiagobaratieri97@gmail.com>
2021-10-01 16:20:37 -0300

    Login backend integration

--- mobile/src/components/HeaderText.tsx
+++ mobile/src/components/HeaderText.tsx
@@ -1,16 +1,24 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { Text, TouchableOpacity } from "react-native";
+import { isAuthenticated } from '../services/auth';
 import { text } from "../styles";
 
 const HeaderText: React.FC = () => {
   const navigation = useNavigation();
 
+  async function handleClick() {
+    const result = await isAuthenticated();
+    result ? navigation.navigate("Movie") : navigation.navigate("Login");
+  }
+
   return (
-    <TouchableOpacity onPress={() => navigation.navigate("Movies")}>
+    <TouchableOpacity onPress={() => handleClick()}>
       <Text style={text.navText}>MovieFlix</Text>
     </TouchableOpacity>
   );
 };
 
 export default HeaderText;
+
+
--- mobile/src/components/Navbar.tsx
+++ mobile/src/components/Navbar.tsx
@@ -1,14 +1,25 @@
 import { useNavigation } from "@react-navigation/native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Text, TouchableOpacity } from "react-native";
+import { doLogout, isAuthenticated } from "../services/auth";
 import { text, theme } from "../styles";
 
 const NavBar: React.FC = () => {
   const navigation = useNavigation();
-  const [authenticated, setAuthenticated] = useState(true);
+  const [authenticated, setAuthenticated] = useState(false);
+
+  async function logged() {
+    const result = await isAuthenticated();
+
+    result ? setAuthenticated(true) : setAuthenticated(false);
+  }
+
+  useEffect(() => {
+    logged();
+  }, [logged]);
 
   function logout() {
-    setAuthenticated(false);
+    doLogout();
     navigation.navigate("Login");
   }
 
